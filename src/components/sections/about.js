2c2a7c40f0c7c85cb15501509857ff210ff35cd8
Mohit Varikuti <65371337+GitHubEmploy@users.noreply.github.com>
2023-08-12 23:21:56 -0700

    Update about.js

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -135,33 +135,14 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Brittany and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              HI! Im Mohit, a 11th-grade student at Dublin High School in California with a deep passion for technology. I have developed my skills in robotics and software development through both my involvement in various clubs and competitions, as well as my extended professional experience.
             </p>
 
             <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              Currently, I do work for a 3d printed company where I use SLS and SLA manufacturing techniques, as well as develop slicing algorithms for non standard FDM techniques. Alongside everything else, I am also passionate about Al and machine learning, with a focus on developing algorithms using Python and R for algorithmic stock trading, as as a general interest.
             </p>
 
-            <p>
-              I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
-              </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
-            </p>
-
-            <p>Here are a few technologies I’ve been working with recently:</p>
+            <p>Here are a few of the cool things I have done over the years:</p>
           </div>
 
           <ul className="skills-list">
