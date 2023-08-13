9a7c87463f032eeba52d18edb6899f529f120708
Mohit varikuti <mohit.varikuti@gmail.com>
2023-08-13 01:58:11 -0700

    pain

--- src/components/footer.js
+++ src/components/footer.js
@@ -105,7 +105,7 @@ const Footer = () => {
       </StyledSocialLinks>
 
       <StyledCredit tabindex="-1">
-        <a href="https://github.com/bchiang7/v4">
+        <a href="">
           <div>Designed with ❤️</div>
 
           {githubInfo.stars && githubInfo.forks && (
