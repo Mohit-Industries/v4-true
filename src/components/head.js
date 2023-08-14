b438b47117e15f1fdc8744fba3d00e704d71f33b
Mohit Varikuti <65371337+GitHubEmploy@users.noreply.github.com>
2023-08-13 22:14:15 -0700

    Update head.js

--- src/components/head.js
+++ src/components/head.js
@@ -45,19 +45,12 @@ const Head = ({ title, description, image }) => {
       <html lang="en" />
 
       <meta name="description" content={seo.description} />
-      <meta name="image" content={seo.image} />
 
       <meta property="og:title" content={seo.title} />
       <meta property="og:description" content={seo.description} />
-      <meta property="og:image" content={seo.image} />
       <meta property="og:url" content={seo.url} />
       <meta property="og:type" content="website" />
 
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:creator" content={twitterUsername} />
-      <meta name="twitter:title" content={seo.title} />
-      <meta name="twitter:description" content={seo.description} />
-      <meta name="twitter:image" content={seo.image} />
     </Helmet>
   );
 };
