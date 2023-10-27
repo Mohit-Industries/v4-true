14d12d96fe2061c365e3b9d79e836f55c0852781
Mohit Varikuti <65371337+GitHubEmploy@users.noreply.github.com>
2023-10-26 23:34:42 -0700

    Update loader.js

--- src/components/icons/loader.js
+++ src/components/icons/loader.js
@@ -4,7 +4,7 @@ const IconLoader = () => (
   <svg id="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
     <title>Loader Logo</title>
     <g>
-      <text x="33.7" y="70" fill="currentColor" fontSize="55px" fontFamily="Consolas, serif">
+      <text x="50%" y="70" fill="currentColor" fontSize="55px" fontFamily="Consolas, serif" textAnchor="middle">
         M
       </text>
       <path
