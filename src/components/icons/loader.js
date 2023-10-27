a6d46fe9862a2d17a0679f85a08106e83a7dade6
Mohit Varikuti <65371337+GitHubEmploy@users.noreply.github.com>
2023-10-26 23:27:22 -0700

    Update loader.js

--- src/components/icons/loader.js
+++ src/components/icons/loader.js
@@ -1,24 +1,26 @@
 import React from 'react';
 
-const IconLogo = () => (
-  <svg id="logo" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96">
-    <title>Logo</title>
-    <g transform="translate(-8.000000, -2.000000)">
-      <g transform="translate(11.000000, 5.000000)">
-        <polygon
-          id="Shape"
-          stroke="currentColor"
-          strokeWidth="5"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          points="39 0 0 22 0 67 39 90 78 68 78 23"
-        />
-      </g>
+const IconLoader = () => (
+  <svg id="logo" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
+    <title>Loader Logo</title>
+    <g>
+      <text x="33.7" y="70" fill="currentColor" fontSize="55px" fontFamily="Consolas, serif">
+        M
+      </text>
+      <path
+        stroke="currentColor"
+        strokeWidth="5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M 50, 5
+           L 11, 27
+           L 11, 72
+           L 50, 95
+           L 89, 73
+           L 89, 28 z"
+      />
     </g>
-    <text x="28" y="65" fill="currentColor" fontSize="50px" fontFamily="Consolas, serif">
-      M
-    </text>
   </svg>
 );
 
-export default IconLogo;
+export default IconLoader;
